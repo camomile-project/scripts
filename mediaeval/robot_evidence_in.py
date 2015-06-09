32b482a87697eeebb2d4fb7b9b60274e0925c00e
Herve Bredin <bredin@limsi.fr>
2015-06-09 16:45:00 +0200

    feat: robot_evidence_in and robot_evidence_out

---
+++ mediaeval/robot_evidence_in.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 CNRS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+# AUTHORS
+# Hervé BREDIN -- http://herve.niderb.fr/
+
+"""
+MediaEval evidence in
+
+Usage:
+  robot_evidence_in [options]
+
+Options:
+  -h --help                Show this screen.
+  --version                Show version.
+  --debug                  Show debug information.
+  --url=URL                Submission server URL
+                           [default: http://api.mediaeval.niderb.fr]
+  --password=P45sw0Rd      Password
+
+"""
+
+from common import RobotCamomile
+from docopt import docopt
+from getpass import getpass
+
+arguments = docopt(__doc__, version='0.1')
+
+url = arguments['--url']
+password = arguments['--password']
+
+robot = RobotCamomile(url, 'robot_evidence', password=password)
+
+# filled by robot_submission
+# {evidence: id_evidence, label: id_label}
+evidenceSubmissionQueue = robot.getQueueByName(
+    'mediaeval.submission.evidence.in')
+
+# filled by this script and popped by evidence annotation front-end
+evidenceInQueue = robot.getQueueByName(
+    'mediaeval.evidence.in')
+
+# unique layer containing manual annotations
+test = robot.getCorpusByName('mediaeval.test')
+evidenceGroundtruthLayer = robot.getLayerByName(
+    test, 'mediaeval.groundtruth.evidence.all')
+
+# layer containing submission shots
+submissionShotLayer = robot.getLayerByName(test, 'mediaeval.submission_shot')
+
+# forever loop on evidence submission queue
+for submissionLayers in robot.dequeue_loop(evidenceSubmissionQueue):
+
+    # evidenceSubmissionQueue is filled by submission script
+    # {evidence: id_evidence, label: id_label}
+    evidenceHypothesisLayer = submissionLayers.evidence
+    id_submission = submissionLayers.label
+
+    # keep track of (already done) manual annotations
+    # {id_shot, person_name, source: corrected_person_name}
+    # {id_shot, person_name, source: False} (if not an evidence)
+    mapping = {}
+    for _, evidences in robot.getAnnotations_iter(evidenceGroundtruthLayer):
+        for evidence in evidences:
+            id_shot = evidence.fragment
+            person_name = evidence.data.person_name
+            source = evidence.data.source
+            to = (evidence.data.corrected_person_name
+                  if evidence.data.is_evidence
+                  else False)
+            mapping[id_shot, person_name, source] = to
+
+    # process evidence layer medium by medium
+    for id_medium, evidences in robot.getAnnotations_iter(
+            evidenceHypothesisLayer):
+
+        # loop on evidence from one medium
+        for evidence in evidences:
+
+            id_shot = evidence.fragment
+            person_name = evidence.data.person_name
+            source = evidence.data.source
+
+            # if this hypothesized evidence has been checked already
+            if (id_shot, person_name, source) in mapping:
+
+                # propagate this evidence to this submission mapping
+                description = robot.getLayer(id_submission).description
+                # (initialize empty mapping if needed)
+                _ = description.setdefault('mapping', {})
+                description.mapping[person_name] = mapping[id_shot,
+                                                           person_name,
+                                                           source]
+                robot.updateLayer(id_submission, description=description)
+
+            # if this hypothesized evidence has not been checked yet
+            # push to evidence annotation frontend
+            else:
+
+                item = {}
+
+                item['id_submission'] = id_submission
+                item['person_name'] = person_name
+                item['source'] = source
+
+                item['id_medium'] = id_medium
+                item['id_shot'] = id_shot
+                segment = robot.getAnnotation(id_shot).fragment.segment
+                item['start'] = segment.start - (5 if source == 'audio'
+                                                 else 0)
+                item['end'] = segment.end + (5 if source == 'audio'
+                                             else 0)
+
+                robot.enqueue(evidenceInQueue, item)
+
+                print "new evidence - {name:s}".format(name=person_name)
---
+++ mediaeval/robot_evidence_out.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 CNRS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+# AUTHORS
+# Hervé BREDIN -- http://herve.niderb.fr/
+
+"""
+MediaEval submission robot
+
+Usage:
+  robot_submission [options]
+
+Options:
+  -h --help                Show this screen.
+  --version                Show version.
+  --debug                  Show debug information.
+  --url=URL                Submission server URL
+                           [default: http://api.mediaeval.niderb.fr]
+  --password=P45sw0Rd      Password
+
+"""
+
+from common import RobotCamomile
+from docopt import docopt
+
+arguments = docopt(__doc__, version='0.1')
+
+url = arguments['--url']
+password = arguments['--password']
+
+robot = RobotCamomile(url, 'robot_evidence', password=password)
+
+# filled by evidence annotation front-end
+evidenceOutQueue = robot.getQueueByName(
+    'mediaeval.evidence.out')
+
+# unique layer containing manual annotations
+test = robot.getCorpusByName('mediaeval.test')
+evidenceGroundtruthLayer = robot.getLayerByName(
+    test, 'mediaeval.groundtruth.evidence.all')
+
+# keep track of (already done) manual annotations
+# {id_shot, person_name, source: corrected_person_name}
+# {id_shot, person_name, source: False} (if not an evidence)
+mapping = {}
+for _, evidences in robot.getAnnotations_iter(evidenceGroundtruthLayer):
+    for evidence in evidences:
+        id_shot = evidence.fragment
+        person_name = evidence.data.person_name
+        source = evidence.data.source
+        to = (evidence.data.corrected_person_name
+              if evidence.data.is_evidence
+              else False)
+        mapping[id_shot, person_name, source] = to
+
+# forever loop on evidence front-end output
+for item in robot.dequeue_loop(evidenceOutQueue):
+
+    # front-end input
+    id_shot = item.input.id_shot
+    id_medium = item.input.id_medium
+    id_submission = item.input.id_submission
+    person_name = item.input.person_name
+    source = item.input.source
+
+    # front-end output
+    is_evidence = item.output.is_evidence
+    if is_evidence:
+        corrected_person_name = item.output.person_name
+        seconds = item.output.time
+        bounding_box = item.output.bounding_box
+
+    # if this hypothesized evidence has not been checked yet
+    # store it to the manual annotation layer
+    if (id_shot, person_name, source) not in mapping:
+
+        data = {}
+        data['person_name'] = person_name
+        data['is_evidence'] = is_evidence
+        data['source'] = source
+        if is_evidence:
+            data['corrected_person_name'] = corrected_person_name
+            data['mugshot'] = {}
+            data['mugshot']['time'] = seconds
+            data['mugshot']['bounding_box'] = bounding_box
+
+        robot.createAnnotation(evidenceGroundtruthLayer,
+                               medium=id_medium,
+                               fragment=id_shot,
+                               data=data)
+
+        # remember we have just added this new annotation
+        mapping[id_shot, person_name, source] = (corrected_person_name
+                                                 if is_evidence
+                                                 else False)
+
+        print "new evidence - {name:s}".format(name=person_name)
+
+    # propagate this evidence to the corresponding submission mapping
+    description = robot.getLayer(id_submission).description
+    # (initialize empty mapping if needed)
+    _ = description.setdefault('mapping', {})
+    description.mapping[person_name] = mapping[id_shot,
+                                               person_name,
+                                               source]
+    robot.updateLayer(id_submission, description=description)
