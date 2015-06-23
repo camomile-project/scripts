8f38963e1a5823820a067ab8fbf6dfc9c1124ae9
Herve Bredin <bredin@limsi.fr>
2015-06-23 15:11:29 +0200

    fix: do not wait twice

--- robot_evidence_in.py
+++ robot_evidence_in.py
@@ -50,7 +50,6 @@ Options:
 from common import RobotCamomile, create_logger
 from docopt import docopt
 from time import sleep
-from datetime import datetime
 
 arguments = docopt(__doc__, version='0.1')
 
@@ -77,15 +76,18 @@ evidenceGroundtruthLayer = robot.getLayerByName(
     test, 'mediaeval.groundtruth.evidence.all')
 
 
-def update(nbAnnotationInQueue):
-    # for all hypothesis layer
+def update(numberOfEvidencesInQueue):
 
-    items = []
-    if nbAnnotationInQueue > limit:
-        return items
+    # do not add any new evidence if the queue is already full
+    newEvidences = []
+    if numberOfEvidencesInQueue > limit:
+        return newEvidences
 
     logger.debug("loading list of existing evidences")
 
+    # keep track of (already done) manual annotations
+    # {id_shot, person_name, source: corrected_person_name}
+    # {id_shot, person_name, source: False} (if not an evidence)
     mapping = {}
     for _, evidences in robot.getAnnotations_iter(evidenceGroundtruthLayer):
         for evidence in evidences:
@@ -97,6 +99,7 @@ def update(nbAnnotationInQueue):
                   else False)
             mapping[id_shot, person_name, source] = to
 
+    # for all evidence layers
     for layer in robot.getLayers(
             test, data_type='mediaeval.persondiscovery.evidence'):
 
@@ -108,7 +111,7 @@ def update(nbAnnotationInQueue):
         if 'deleted' in layer.description:
             continue
 
-        # skip if all annotations are done
+        # skip if all evidences are already checked
         if 'annotationsComplete' in layer.description:
             continue
 
@@ -117,34 +120,32 @@ def update(nbAnnotationInQueue):
         labelLayer = layer.description.id_label
 
         description = robot.getLayer(labelLayer).description
-        # (initialize empty mapping if needed)
         description.setdefault('mapping', {})
-        # keep track of (already done) manual annotations
-        # {id_shot, person_name, source: corrected_person_name}
-        # {id_shot, person_name, source: False} (if not an evidence)
 
-        annotationToDo = False
+        # change this to true if at least one evidence needs to be checked
+        hasUncheckedEvidences = False
 
+        # loop on all hypothesized evidences
         for id_medium, evidences in robot.getAnnotations_iter(evidenceLayer):
-
-            # loop on evidence from one medium
             for evidence in evidences:
 
+                # get details abouth the evidence
                 id_shot = evidence.fragment
                 person_name = evidence.data.person_name
                 source = evidence.data.source
 
+                # if this evidence has been checked already (locally)
+                # no need to check it twice
                 if person_name in description.mapping:
                     continue
 
-                # if this hypothesized evidence has been checked already
+                # if this evidence has been checked already (globally)
+                # add it to the local mapping
                 if (id_shot, person_name, source) in mapping:
 
                     logger.debug(
                         "existing evidence - {name:s} - {source:s}".format(
                             name=person_name, source=source))
-
-                    # propagate this evidence to this submission mapping
 
                     description.mapping[person_name] = mapping[id_shot,
                                                                person_name,
@@ -155,7 +156,7 @@ def update(nbAnnotationInQueue):
                 # push to evidence annotation frontend
                 else:
 
-                    annotationToDo = True
+                    hasUncheckedEvidences = True
 
                     item = {}
 
@@ -171,36 +172,38 @@ def update(nbAnnotationInQueue):
                     item['end'] = segment.end + (5 if source == 'audio'
                                                  else -0.5)
 
-                    items.append(item)
+                    newEvidences.append(item)
 
-                    if len(items) > limit - nbAnnotationInQueue:
-                        return items
+                    # stop gathering evidences as soon as we expect to fill
+                    # the evidence annotation frontend queue
+                    if len(newEvidences) + numberOfEvidencesInQueue > limit:
+                        return newEvidences
 
-        if not annotationToDo:
+        # if all evidences have been checked, mark layer as such
+        if not hasUncheckedEvidences:
             description = layer.description
             description['annotationsComplete'] = True
             robot.updateLayer(evidenceLayer, description=description)
-
-            logger.debug("all evidences are annotated for {layer:s}".format(
+            logger.debug("all evidences were checked for {layer:s}".format(
                          layer=evidenceLayer))
 
-    return items
+    return newEvidences
 
 
-t = datetime.now()
 while True:
 
-    items = update(robot.pickLength(evidenceInQueue))
-    if not items:
-        sleep(period)
+    # get a certain amount of new evidences to annotate
+    # based on the current length of the queue
+    newEvidences = update(robot.pickLength(evidenceInQueue))
 
-    for item in items:
+    # add (and log) all evidences, the one after the other
+    for item in newEvidences:
         logger.info(
             "new evidence - {name:s} - {source:s}".format(
                 name=item['person_name'], source=item['source']))
 
-        robot.enqueue_fair(evidenceInQueue, item, limit=limit)
+        robot.enqueue(evidenceInQueue, item)
 
-        if (datetime.now() - t).total_seconds() > period:
-            t = datetime.now()
-            break
+    # if there are no new evidences, wait for a while
+    if not newEvidences:
+        sleep(period)
